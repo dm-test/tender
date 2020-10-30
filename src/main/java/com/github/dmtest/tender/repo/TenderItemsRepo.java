dd1231b45b9c0e0305d4e1f6880233d20d9d2243
Dmitry Piskunov <dm.test@mail.ru>
2020-10-30 18:38:21 +0300

    Переработка работы с БД: удаление ненужных классов

--- src/main/java/com/github/dmtest/tender/repo/TenderItemsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.github.dmtest.tender.repo;
-
-import com.github.dmtest.tender.domain.TenderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TenderItemsRepo extends JpaRepository<TenderItem, UUID> {
-}
--- src/main/java/com/github/dmtest/tender/repo/TendersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.github.dmtest.tender.repo;
-
-import com.github.dmtest.tender.domain.Tender;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TendersRepo extends JpaRepository<Tender, UUID> {
-}
